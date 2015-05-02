a31c2a55374aa0d029d0372a324a1c99b5aced2c
Noriyuki Takarabe <junk.freak.net@gmail.com>
2015-05-02 18:53:49 +0900

    2015/05/02(sat) 18:53 commit and push on envy.

--- commons/src/mycommons/constants/db/sql/ddl/FieldTypes.java
+++ commons/src/mycommons/constants/db/sql/ddl/FieldTypes.java
@@ -9,33 +9,36 @@ public class FieldTypes {
 	public static final String BIT="BIT";
 	public static final String BLOB="BLOB";
 	public static final String BOOLEAN="BOOLEAN";
-	CHAR
-	CLOB
-	DATALINK
-	DATE
-	DECIMAL
-	DISTINCT
-	FLOAT
-	INTEGER
-	JAVA_OBJECT
-	LONGNVARCHAR
-	LONGVARBINARY
-	LONGVARCHAR
-	NCHAR
-	NCLOB
-	NULL
-	NUMERIC
-	NVARCHAR
-	OTHER
-	REAL
-	REF
-	ROWID
-	SMALLINT
-	SQLXML
-	STRUCT
-	TIME
-	TIMESTAMP
-	TINYINT
-	VARBINARY
-	VARCHAR
+	public static final String CHAR="CHAR";
+	public static final String CLOB="CLOB";
+	public static final String DATALINK="DATALINK";
+	public static final String DATE="DATE";
+	public static final String DECIMAL="DECIMAL";
+	public static final String DISTINCT="DISTINCT";
+	public static final String FLOAT="FLOAT";
+	public static final String INTEGER="INTEGER";
+	public static final String JAVA_OBJECT="JAVA_OBJECT";
+	public static final String LONGNVARCHAR="LONGNVARCHAR";
+	public static final String LONGVARBINARY="LONGVARBINARY";
+	public static final String LONGVARCHAR="LONGVARCHAR";
+	public static final String NCHAR="NCHAR";
+	public static final String NCLOB="NCLOB";
+	public static final String NULL="NULL";
+	public static final String NUMERIC="NUMERIC";
+	public static final String NVARCHAR="NVARCHAR";
+	public static final String OTHER="OTHER";
+	public static final String REAL="REAL";
+	public static final String REF="REF";
+	public static final String SMALLINT="SMALLINT";
+	public static final String SQLXML="SQLXML";
+	public static final String STRUCT="STRUCT";
+	public static final String TIME="TIME";
+	public static final String TIMESTAMP="TIMESTAMP";
+	public static final String TINYINT="TINYINT";
+	public static final String VARBINARY="VARBINARY";
+	public static final String VARCHAR="VARCHAR";
+	
+	//
+	public static final String UNKNOWN="UNKNOWN";
+	
 }
--- commons/src/mycommons/db/FieldTypeSQL.java
+++ commons/src/mycommons/db/FieldTypeSQL.java
@@ -15,7 +15,7 @@ public class FieldTypeSQL {
 		this.constructorCommon(in_typesql);
 	}
 	public FieldTypeSQL(mycommons.db.FieldType in_type){
-		
+		this.constructorCommon(in_type);
 	}
 	//constructor common
 	private void constructorCommon(String in_typesql){
@@ -30,6 +30,67 @@ public class FieldTypeSQL {
 	
 	//method private
 	private String getTypeSQLString(mycommons.db.FieldType in_type){
+		
+		int type_i=in_type.getType();
+		String rv=mycommons.constants.Generic.CS_SPACE;
+		
+		switch(type_i){
+
+		case java.sql.Types.ARRAY:
+			rv=mycommons.constants.db.sql.ddl.FieldTypes.ARRAY;
+			break;
+		case java.sql.Types.BIGINT:
+			rv=mycommons.constants.db.sql.ddl.FieldTypes.BIGINT;
+			break;
+		case java.sql.Types.BINARY:
+			rv=mycommons.constants.db.sql.ddl.FieldTypes.BINARY;
+			break;
+		case java.sql.Types.BIT:
+			rv=mycommons.constants.db.sql.ddl.FieldTypes.BIT;
+			break;
+		case java.sql.Types.BLOB:
+			rv=mycommons.constants.db.sql.ddl.FieldTypes.BLOB;
+			break;
+		case java.sql.Types.BOOLEAN:
+			rv=mycommons.constants.db.sql.ddl.FieldTypes.BOOLEAN;
+			break;
+		default:
+			rv=mycommons.constants.db.sql.ddl.FieldTypes.UNKNOWN;
+			break;
+			
+		}
+
+		CHAR
+		CLOB
+		DATALINK
+		DATE
+		DECIMAL
+		DISTINCT
+		FLOAT
+		INTEGER
+		JAVA_OBJECT
+		LONGNVARCHAR
+		LONGVARBINARY
+		LONGVARCHAR
+		NCHAR
+		NCLOB
+		NULL
+		NUMERIC
+		NVARCHAR
+		OTHER
+		REAL
+		REF
+		ROWID
+		SMALLINT
+		SQLXML
+		STRUCT
+		TIME
+		TIMESTAMP
+		TINYINT
+		VARBINARY
+		VARCHAR		
+		return rv;
+
 		
 	}
 	//method public
