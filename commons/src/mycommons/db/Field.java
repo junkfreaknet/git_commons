b607f90f0b6586e2755b58ee9ced5ce7bad3b6f9
junkfreaknet <junk.freak.net@gmail.com>
2015-05-03 20:44:47 +0900

    2015/05/03 20:44 commit and push on envy.

--- commons/src/mycommons/db/Field.java
+++ commons/src/mycommons/db/Field.java
@@ -4,24 +4,24 @@ public class Field {
 
 	private mycommons.db.FieldName name;
 	private mycommons.db.FieldType type;
-	private mycommons.db.FieldTypeSQL typeSQL;
+	//private mycommons.db.FieldTypeSQL typeSQL;
 
 	
 	//constructors
 	public Field(){
 		
-		this.constructorCommon(new mycommons.db.FieldName(),new mycommons.db.FieldType(),new mycommons.db.FieldTypeSQL());
+		this.constructorCommon(new mycommons.db.FieldName(),new mycommons.db.FieldType());
 	}
-	public Field(mycommons.db.FieldName in_fieldname,mycommons.db.FieldType in_fieldtype,mycommons.db.FieldTypeSQL in_typesql){
-		this.constructorCommon(in_fieldname, in_fieldtype,in_typesql);
+	public Field(mycommons.db.FieldName in_fieldname,mycommons.db.FieldType in_fieldtype){
+		this.constructorCommon(in_fieldname, in_fieldtype);
 	}
 
 	//constructor common
-	private void constructorCommon(mycommons.db.FieldName in_fieldname,mycommons.db.FieldType in_fieldtype,mycommons.db.FieldTypeSQL in_typesql){
+	private void constructorCommon(mycommons.db.FieldName in_fieldname,mycommons.db.FieldType in_fieldtype){
 		
 		this.name=in_fieldname;
 		this.type=in_fieldtype;
-		this.typeSQL=in_typesql;
+		//this.typeSQL=in_typesql;
 		
 	}
 	
@@ -32,12 +32,14 @@ public class Field {
 	public void setType(mycommons.db.FieldType in_type){
 		this.type=in_type;
 	}
+	/***
 	public void setTypeSQL(mycommons.db.FieldTypeSQL in_typesql){
 		this.typeSQL=in_typesql;
 	}
 	public void setTypeSQL(mycommons.db.FieldType in_type){
 		
 	}
+	***/
 	//getter
 	public mycommons.db.FieldName getName(){
 		return this.name;
@@ -45,7 +47,12 @@ public class Field {
 	public mycommons.db.FieldType getType(){
 		return this.type;
 	}
+	public mycommons.db.FieldTypeSQL getTypeSql(){
+	
+	}
+	/***
 	public mycommons.db.FieldTypeSQL getTypeSQL(){
 		return this.typeSQL;
 	}
+	***/
 }
