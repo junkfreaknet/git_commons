c023c7e6f57a31db5bf96290bf251e6722cd6f2b
Noriyuki Takarabe <junk.freak.net@gmail.com>
2015-05-09 03:50:44 +0900

    2015/05/09(sat) 03:50 commit and push on envy. add a module of SQL command

---
+++ commons/src/mycommons/constants/db/sql/ddl/Commands.java
@@ -0,0 +1,19 @@
+package mycommons.constants.db.sql.ddl;
+
+public class Commands {
+
+	//sql ddl commands
+	public static final String CREATE_TABLE="CREATE TABLE ";
+	
+	//*******************************************************
+	//generics
+	//*******************************************************
+	//fields start
+	public static final String FIELDS_START="(";
+	//fields end
+	public static final String FIELDS_END=")";
+	//blank
+	public static final String SPACE=mycommons.constants.Generic.CS_ONE_BLANK;
+	//","
+	public static final String FIELD_SEPARATOR=",";
+}
--- commons/src/mycommons/routines/db/Definition.java
+++ commons/src/mycommons/routines/db/Definition.java
@@ -2,9 +2,38 @@ package mycommons.routines.db;
 
 public class Definition {
 
-	public static mycommons.db.SQLString createSqlStringCreateTable(){
+	//****************************************************************
+	//generate a "create table " SQL string
+	//****************************************************************
+	public static mycommons.db.SQLString createSqlStringCreateTable(mycommons.db.Table in_table,java.util.ArrayList<mycommons.db.Field> in_fields){
+
+		String sql=mycommons.constants.Generic.CS_SPACE;
 		
-		mycommons.db.SQLString rv=new mycommons.db.SQLString();
+		//first
+		sql=sql+mycommons.constants.db.sql.ddl.Commands.CREATE_TABLE+in_table.getName()+mycommons.constants.db.sql.ddl.Commands.FIELDS_START;
+		//fields
+		for(int i=0;i<in_fields.size();i++){
+			sql=sql+createSqlStringCreateTable_a_Field(in_fields.get(i));
+			if(i==in_fields.size()-1){
+				
+			}else{
+				sql=sql+mycommons.constants.db.sql.ddl.Commands.FIELD_SEPARATOR;
+			}
+		}
+		//end
+		sql=sql+mycommons.constants.db.sql.ddl.Commands.FIELDS_END;
+		//at last
+		mycommons.db.SQLString rv=new mycommons.db.SQLString(sql);
+		return rv;
+	}
+	//generate string for a field.
+	private static String createSqlStringCreateTable_a_Field(mycommons.db.Field in_field){
+		
+		String rv=mycommons.constants.Generic.CS_SPACE;
+		//name
+		rv=rv+in_field.getName().getName()+mycommons.constants.db.sql.ddl.Commands.SPACE;
+		//type
+		rv=rv+in_field.getTypeSQL().getTypeSQLString(in_field.getType());
 		
 		return rv;
 	}
