10f4817263ef19efb814f24c2d20fb4e5aa3adc3
Noriyuki Takarabe <junk.freak.net@gmail.com>
2015-05-27 03:20:32 +0900

    2015/05/27(wed) 03:18 commit and push on envy. count of records limitation is done sucessfuly.

---
+++ commons/src/mycommons/routines/generic/Compare.java
@@ -0,0 +1,20 @@
+package mycommons.routines.generic;
+
+public class Compare {
+
+	//compare integer 
+	//
+	public static boolean isEqual(int i,int j){
+		
+		boolean rv=false;
+		
+		Integer a=i;
+		Integer b=j;
+		int result;
+		result=a.compareTo(b);
+		if(result==0){
+			rv=true;
+		}
+		return rv;
+	}
+}
